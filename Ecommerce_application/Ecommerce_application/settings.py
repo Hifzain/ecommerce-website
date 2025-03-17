59a4b65d94ab72592c3d764d58cbcc23dee924bc
Hifzain <syedhifzain280@gmail.com>
2025-03-17 10:48:49 -0700

    Update settings.py

--- Ecommerce_application/Ecommerce_application/settings.py
+++ Ecommerce_application/Ecommerce_application/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!qz&=_wv6s69!x*p&8bj@#b8)9u966m581+=jtq(khvcwdo_7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SolarTech.onrender.com']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiceMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,13 +133,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+DEBUG = False
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'debug',
